63e4db698e66569ea3c59eb2a25dd7daeaf25149
tsk takezawa <tsk.take815@gmail.com>
2017-03-04 16:45:03 +0900

    Create chat like UI

--- SampleChat/ViewController.swift
+++ SampleChat/ViewController.swift
@@ -2,7 +2,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet fileprivate weak var textView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+}
+
+extension ViewController {
+
+    @IBAction func didTouchPostButton(_ sender: UIButton) {
+        print("didTap: \(textView.text)")
+        textView.text = nil
+        textView.resignFirstResponder()
     }
 }
